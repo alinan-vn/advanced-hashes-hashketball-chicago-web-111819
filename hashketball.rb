d0920230f1c45a56b0f4478254f3229bb7eaf809
Alejandro Linan <jandrolinan@gmail.com>
2019-11-16 03:47:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,7 +59,7 @@ def game_hash
         }]
       },
     :away => {
-      :team_name => "Charolotte Hornets",
+      :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [{
         :player_name => "Jeff Adrien",
